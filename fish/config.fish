a93430f323517d909f7027b19b30f6c780399acf
carlosmza <carlosmendozajmz@gmail.com>
2026-06-09 11:14:48 -0600

    feat: eza aliases

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,20 @@
 ~/.local/bin/oh-my-posh init fish --config '~/.config/oh-my-posh/current.json' | source
 zoxide init fish | source
 set -gx EDITOR nvim
+
+# opencode
+fish_add_path /home/carlosm/.opencode/bin
+if status is-interactive
+    if not set -q ZELLIJ
+        if test "$TERM" = "foot"
+            exec zellij
+        end
+    end
+end
+# Inicializar Atuin para el historial interactivo
+atuin init fish | source
+# Reemplazos de ls con eza (con colores e iconos)
+alias ls="eza --color=always --icons=always --group-directories-first"
+alias la="eza --color=always --icons=always --group-directories-first -a --long --header --git"
+alias tree="eza --color=always --icons=always --group-directories-first --tree"
+alias y="yazi"
