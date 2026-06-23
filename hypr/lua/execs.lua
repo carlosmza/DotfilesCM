f4927c479940a1576004bcf1dc3c0509a67da897
carlosmza <carlosmendozajmz@gmail.com>
2026-06-23 11:41:51 -0600

    feat: fix appearance dark/light

--- hypr/lua/execs.lua
+++ hypr/lua/execs.lua
@@ -2,6 +2,8 @@
 
 hl.on("hyprland.start", function()
     hl.exec_cmd("qs")
+		hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP ")
+		hl.exec_cmd("systemctl --user start hyprland-session.target")
     hl.exec_cmd("/usr/bin/awww-daemon")
     -- hl.exec_cmd("kitty --listen-on unix:/tmp/kitty.sock --single-instance")
     hl.exec_cmd("wl-paste --watch cliphist store")
@@ -9,3 +11,6 @@ hl.on("hyprland.start", function()
 end)
 
 hl.exec_cmd("/home/carlosm/.config/scripts/system/refresh-display.sh")
+-- exec-once = dbus-update-activation-environment --systemd --all
+-- exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_TYPE XDG_SESSION_DESKTOP
+-- exec-once = systemctl --user start hyprland-session.target
