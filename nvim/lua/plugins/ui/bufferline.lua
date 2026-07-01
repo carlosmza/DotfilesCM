d6b88ad4cf26d70cab5125be1de7088b95e42a4c
carlosmza <carlosmendozajmz@gmail.com>
2026-07-01 13:02:25 -0600

    feat: number tab

--- nvim/lua/plugins/ui/bufferline.lua
+++ nvim/lua/plugins/ui/bufferline.lua
@@ -1,12 +1,14 @@
 return {
   "akinsho/bufferline.nvim",
+	lazy = false,
+	priority = 900,
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     require("bufferline").setup {
       options = {
         mode = "buffers", -- o "tabs"
-        numbers = "buffer_id", -- o "ordinal", "buffer_id", "both"
+        numbers = "ordinal", -- o "ordinal", "buffer_id", "both"
         diagnostics = "nvim_lsp",
         separator_style = "padded_slant", -- "slant", "padded_slant", "thin", etc
         show_close_icon = false,
