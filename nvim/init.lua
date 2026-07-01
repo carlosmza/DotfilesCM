00c79c50dc8013f02839fc7cfaa369b6fbbfb26b
carlosmza <carlosmendozajmz@gmail.com>
2026-07-01 13:01:41 -0600

    fix: colorscheme with var

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,25 +2,41 @@ require("vim._core.ui2").enable({})
 require ("config.lazy")
 require ("config.keymaps")
 require ("config.options")
+
 vim.g.suda_smart_edit = 1  -- Detecta automáticamente si necesita sudo
--- Colores predefinidos
-local light_scheme = "rose-pine-dawn"   -- cámbialo a tu gusto
-local dark_scheme = "gruvbox"  -- cámbialo a tu gusto
-local json_path = "/home/carlosm/.config/system-themes/themes/current.json"
 
--- Función que lee el valor "variant" con jq y aplica el tema
+local colorscheme_file = vim.fn.expand("~/.config/nvim/.colorscheme")
+
+local theme_map = {
+-- "Theme name": "colorscheme name"
+	ashes    = "noirbuddy",
+  ["gruvbox-dark-medium"]  = "gruvbox",
+  ["gruvbox-light-soft"]  = "gruvbox",
+  ["rose-pine-dawn"]    = "rose-pine-dawn",
+  ["tokyo-night-dark"]    = "tokyonight-night",
+}
+
 local function aplicar_tema()
-  local resultado = vim.fn.system(string.format("jq -r .variant %s", json_path))
-  local variant = vim.trim(resultado)  -- quitar salto de línea final
+  local file = io.open(colorscheme_file, "r")
+  if not file then
+    vim.notify(".colorscheme no encontrado en " .. colorscheme_file, vim.log.levels.WARN)
+    return
+  end
 
-  if variant == "light" then
-    vim.cmd("colorscheme " .. light_scheme)
-  elseif variant == "dark" then
-    vim.cmd("colorscheme " .. dark_scheme)
+  local key = vim.trim(file:read("*a") or "")
+  file:close()
+
+  if key == "" then
+    vim.notify(".colorscheme está vacío", vim.log.levels.WARN)
+    return
+  end
+
+  local scheme = theme_map[key:lower()]
+  if scheme then
+    vim.cmd("colorscheme " .. scheme)
   else
-    vim.notify("Valor inesperado de 'variant': " .. variant, vim.log.levels.WARN)
+    vim.notify("Tema no mapeado: " .. key, vim.log.levels.WARN)
   end
 end
 
--- Aplicar el tema ya que inicialmente el archivo puede tener un valor
 aplicar_tema()
