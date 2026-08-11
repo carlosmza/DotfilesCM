7d77a960fce2d7d4455175e592271bf0693198ca
carlosmza <carlosmendozajmz@gmail.com>
2026-08-11 09:00:47 -0600

    feat: zellij optional

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,6 @@
 # if status is-interactive
 # # Commands to run in interactive sessions can go here
 # end
-# export QML2_IMPORT_PATH="$HOME/.config/quickshell/config"
-# source ~/.config/system-themes/env
 ~/.local/bin/oh-my-posh init fish --config '~/.config/oh-my-posh/current.json' | source
 zoxide init fish | source
 set -gx EDITOR nvim
@@ -10,11 +8,12 @@ set -gx EDITOR nvim
 # opencode
 fish_add_path /home/carlosm/.opencode/bin
 # if status is-interactive
-#     if not set -q ZELLIJ
-#         if test "$TERM" = "foot"
-#             exec zellij
-#         end
-#     end
+    # if not set -q ZELLIJ
+        # if test "$TERM" != "foot"
+            # exec zellij
+        # end
+        # exec zellij
+    # end
 # end
 # Inicializar Atuin para el historial interactivo
 atuin init fish | source
